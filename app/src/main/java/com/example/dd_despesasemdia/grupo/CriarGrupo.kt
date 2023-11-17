7ed3677129a345d8ec7686dede7614e9ae42deee
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-17 12:29:25 -0300

    Criar grupo, add pessoa grupo

---
+++ app/src/main/java/com/example/dd_despesasemdia/grupo/CriarGrupo.kt
@@ -0,0 +1,12 @@
+package com.example.dd_despesasemdia.grupo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dd_despesasemdia.R
+
+class CriarGrupo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_grupo)
+    }
+}
