1ceb3f266c77a551dd27453f31574a39f27df151
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-17 10:40:52 -0300

    Layout despesa unica, despesas

---
+++ app/src/main/java/com/example/dd_despesasemdia/individual/DespesaUnica.kt
@@ -0,0 +1,12 @@
+package com.example.dd_despesasemdia.individual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dd_despesasemdia.R
+
+class DespesaUnica : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_despesa_unica)
+    }
+}
---
+++ app/src/main/java/com/example/dd_despesasemdia/individual/LayoutDespesas.kt
@@ -0,0 +1,12 @@
+package com.example.dd_despesasemdia.individual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dd_despesasemdia.R
+
+class LayoutDespesas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_despesas)
+    }
+}
