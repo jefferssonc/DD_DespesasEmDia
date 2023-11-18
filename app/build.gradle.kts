c2cf199b1bda8e63d9d32792e0f37bddf4f8e311
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-18 19:39:21 -0300

    minSdk de 27 para 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.dd_despesasemdia"
-        minSdk = 27
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
