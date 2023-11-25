af3e0b2440dfb96edba1c5df02e7fd455ab039f3
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-25 20:19:11 -0300

    Layout Login criado

---
+++ app/src/main/java/com/example/dd_despesasemdia/LoginLayout.kt
@@ -0,0 +1,11 @@
+package com.example.dd_despesasemdia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_layout)
+    }
+}
