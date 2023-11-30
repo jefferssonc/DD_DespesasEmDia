065e3fbdae0bb2ea327b6ddcfab6d7c0d26f4342
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-30 17:29:12 -0300

    layout criar conta

---
+++ app/src/main/java/com/example/dd_despesasemdia/CriarConta.kt
@@ -0,0 +1,11 @@
+package com.example.dd_despesasemdia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriarConta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_conta)
+    }
+}
